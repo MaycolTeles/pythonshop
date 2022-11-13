650a3d0c06130752b8eee2b97366662a2017b44c
Maycol <maycolteles@hotmail.com>
2022-11-13 00:42:38 -0300

    feat: Adding the run module to execute the application.

---
+++ app/run.py
@@ -0,0 +1,18 @@
+"""
+Module to run the application.
+"""
+
+from src.dependencies import USER_INTERFACE_INJECTION
+
+
+def run() -> None:
+    """
+    Function to run the application.
+    """
+    user_interface = USER_INTERFACE_INJECTION
+    user_interface.execute()
+
+
+
+if __name__ == "__main__":
+    run()
