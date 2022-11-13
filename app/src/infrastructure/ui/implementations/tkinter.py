0343c3b54373c4c6d4d9051915c8e10852ea334e
Maycol <maycolteles@hotmail.com>
2022-11-13 16:18:11 -0300

    feat: Adding new features to tkinter interface.

--- app/src/infrastructure/ui/implementations/tkinter.py
+++ app/src/infrastructure/ui/implementations/tkinter.py
@@ -6,8 +6,7 @@ import pathlib
 
 import customtkinter
 from PIL import ImageTk
-import tkinter
-from tkinter import RIDGE, filedialog, Canvas
+from tkinter import filedialog, Canvas
 
 from src.infrastructure.ui.interfaces import UIInterface
 from src.adapters.controllers.implementations import LoadImageController, RotateImageController
@@ -18,15 +17,11 @@ customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
-CURRENT_PATH = pathlib.Path().resolve()
-
-
 class Tkinter(UIInterface, customtkinter.CTk):
     """
     Class to represent a "Tkinter" UserInterface.
     """
-    WIDTH = 1200
-    HEIGHT = 700
+    CURRENT_PATH = pathlib.Path().resolve()
 
     def execute(self) -> None:
         """"""
@@ -38,12 +33,11 @@ class Tkinter(UIInterface, customtkinter.CTk):
         self._setup()
         self._main_frame_setup()
         self._create_frames()
-        self._set_default_configs()
 
     def _setup(self) -> None:
         """"""
         self.title("PYTHONSHOP")
-        self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+        self.attributes('-zoomed', True)
 
         # call .on_closing() when app gets closed
         self.protocol("WM_DELETE_WINDOW", self._on_closing)
@@ -56,146 +50,162 @@ class Tkinter(UIInterface, customtkinter.CTk):
 
     def _main_frame_setup(self) -> None:
         """"""
-        self.grid_columnconfigure(1, weight=10)
-        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
     def _create_options_frame(self) -> None:
         """"""
         # configure grid layout (1x11)
-        self._frm_options = customtkinter.CTkFrame(
+        frm_options = customtkinter.CTkFrame(
             master=self,
             corner_radius=20
         )
-        self._frm_options.grid(row=0, column=0, pady=10, padx=10, sticky="nsew")
+        frm_options.grid(row=0, column=0, pady=10, padx=10, sticky="ns")
 
-        self._frm_options.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
-        self._frm_options.grid_rowconfigure(1, minsize=20)  # empty row with minsize as spacing
-        self._frm_options.grid_rowconfigure(2, minsize=20)  # empty row with minsize as spacing
-        self._frm_options.grid_rowconfigure(3, weight=1)    # empty row as spacing
-        self._frm_options.grid_rowconfigure(4, minsize=10)  # empty row with minsize as spacing
-        self._frm_options.grid_rowconfigure(5, minsize=20)  # empty row with minsize as spacing
+        frm_options.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
+        frm_options.grid_rowconfigure(1, minsize=20)  # empty row with minsize as spacing
+        frm_options.grid_rowconfigure(2, minsize=20)  # empty row with minsize as spacing
+        frm_options.grid_rowconfigure(3, minsize=20)  # empty row with minsize as spacing
+        frm_options.grid_rowconfigure(4, weight=1)    # empty row as spacing
+        frm_options.grid_rowconfigure(5, minsize=10)  # empty row with minsize as spacing
+        frm_options.grid_rowconfigure(6, minsize=20)  # empty row with minsize as spacing
 
-        self.lbl_options = customtkinter.CTkLabel(
-            master=self._frm_options,
+        lbl_options = customtkinter.CTkLabel(
+            master=frm_options,
             text="OPTIONS",
             text_font=("Roboto Medium", 16) # font name and size in px
         )
-        self.lbl_options.grid(row=0, column=0, pady=10, padx=10)
+        lbl_options.grid(row=0, column=0, pady=10, padx=10)
 
-        self.btn_load_image = customtkinter.CTkButton(
-            master=self._frm_options,
+        btn_load_image = customtkinter.CTkButton(
+            master=frm_options,
             text="Load Image",
             command=self.load_image
         )
-        self.btn_load_image.grid(row=1, column=0, pady=10, padx=20)
+        btn_load_image.grid(row=1, column=0, pady=10, padx=20)
 
-        self.btn_save_image = customtkinter.CTkButton(
-            master=self._frm_options,
+        self.color = btn_load_image.cget('bg')
+
+        btn_save_image = customtkinter.CTkButton(
+            master=frm_options,
             text="Save Image",
             command=self.save_image
         )
-        self.btn_save_image.grid(row=2, column=0, pady=10, padx=20)
+        btn_save_image.grid(row=2, column=0, pady=10, padx=20)
 
-        self.lbl_space = customtkinter.CTkLabel(master=self._frm_options, text="")
-        self.lbl_space.grid(row=3, column=0, pady=0, padx=20, sticky="s")
+        btn_reset_image = customtkinter.CTkButton(
+            master=frm_options,
+            text="Reset Image",
+            command=self.btn_event
+        )
+        btn_reset_image.grid(row=3, column=0, pady=10, padx=20)
 
-        self.lbl_mode = customtkinter.CTkLabel(master=self._frm_options, text="Appearance Mode:")
-        self.lbl_mode.grid(row=4, column=0, pady=0, padx=20, sticky="s")
+        lbl_space = customtkinter.CTkLabel(master=frm_options, text="")
+        lbl_space.grid(row=4, column=0)
 
-        self.mnu_appearance = customtkinter.CTkOptionMenu(
-            master=self._frm_options,
+        lbl_mode = customtkinter.CTkLabel(master=frm_options, text="Appearance Mode:")
+        lbl_mode.grid(row=5, column=0, pady=0, padx=20)
+
+        mnu_appearance = customtkinter.CTkOptionMenu(
+            master=frm_options,
             values=["Dark", "Light", "System"],
             command=self.change_appearance_mode
         )
-        self.mnu_appearance.grid(row=5, column=0, pady=10, padx=20, sticky="w")
+        mnu_appearance.grid(row=6, column=0, pady=(0,20), padx=20)
+        mnu_appearance.set("Dark")
 
     def _create_image_frame(self) -> None:
         """"""
-        self._frm_image = customtkinter.CTkFrame(
+        frm_image = customtkinter.CTkFrame(
             master=self,
             corner_radius=20
         )
-        self._frm_image.grid(row=0, column=1, pady=10, padx=10, sticky="nsew")
+        frm_image.grid(row=0, column=1, pady=10, padx=10, sticky="nswe")
 
-        self._frm_image.rowconfigure(0, weight=1)
-        self._frm_image.columnconfigure(0, weight=1)
+        # frm_image.rowconfigure(0)
+        # frm_image.rowconfigure(1, weight=1)
+        # frm_image.rowconfigure(2)
+        # frm_image.columnconfigure(0, weight=1)
 
-        # TODO: CHECK
-        self.lbl_image = customtkinter.CTkLabel(
-            master=self._frm_image,
+        lbl_image = customtkinter.CTkLabel(
+            master=frm_image,
             text="IMAGE",
-            corner_radius=6,                # <- custom corner radius
-            fg_color=("white", "gray38"),   # <- custom tuple-color
-            justify=tkinter.CENTER
+            text_font=("Roboto Medium", 16) # font name and size in px
         )
-        self.lbl_image.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
+        lbl_image.grid(column=0, row=0, padx=10, pady=10)
 
-        self.slider_1 = customtkinter.CTkSlider(
-            master=self._frm_image,
+        self.canvas = Canvas(frm_image, bg="#2A2D2E", highlightthickness=0)
+        self.canvas.grid(row=1, column=0, sticky="nswe")
+
+        frm_image.grid_rowconfigure(1, weight=1)
+        frm_image.grid_columnconfigure(0, weight=1)
+
+        slider_1 = customtkinter.CTkSlider(
+            master=frm_image,
             from_=0,
             to=1,
             number_of_steps=3,
             command=self.btn_event
         )
-        self.slider_1.grid(row=4, column=0, columnspan=2, pady=10, padx=20, sticky="we")
+        slider_1.grid(row=2, column=0, pady=10, padx=20)
+        slider_1.set(0.2)
 
-        self.slider_2 = customtkinter.CTkSlider(
-            master=self._frm_image,
+        slider_2 = customtkinter.CTkSlider(
+            master=frm_image,
             command=self.btn_event
         )
-        self.slider_2.grid(row=5, column=0, columnspan=2, pady=10, padx=20, sticky="we")
+        slider_2.grid(row=3, column=0, columnspan=2, pady=10, padx=20)
+        slider_2.set(0.7)
 
     def _create_tools_frame(self) -> None:
         """"""
-        self._frm_tools = customtkinter.CTkFrame(
+        frm_tools = customtkinter.CTkFrame(
             master=self,
             corner_radius=20
         )
-        self._frm_tools.grid(row=0, column=2, sticky="nswe", padx=10, pady=10)
+        frm_tools.grid(row=0, column=2, pady=10, padx=10, sticky="ns")
 
         # TODO: CHECK
         # configure grid layout (3x7)
-        # self._frm_tools.rowconfigure((0, 1, 2, 3), weight=1)
-        # self._frm_tools.rowconfigure(7, weight=10)
-        # self._frm_tools.columnconfigure((0, 1), weight=1)
-        # self._frm_tools.columnconfigure(2, weight=0)
+        # frm_tools.rowconfigure((0, 1, 2, 3), weight=1)
+        # frm_tools.rowconfigure(7, weight=10)
+        # frm_tools.columnconfigure((0, 1), weight=1)
+        # frm_tools.columnconfigure(2, weight=0)
 
-        self.lbl_rotate = customtkinter.CTkLabel(
-            master=self._frm_tools,
+        lbl_rotate = customtkinter.CTkLabel(
+            master=frm_tools,
             text="TOOLS",
             text_font=("Roboto Medium", 16) # font name and size in px
         )
-        self.lbl_rotate.grid(row=0, column=0, pady=20, padx=10)
+        lbl_rotate.grid(row=0, column=0, padx=20, pady=10)
 
-        self.lbl_rotate = customtkinter.CTkLabel(
-            master=self._frm_tools,
-            text="Rotate",
-            text_font=(16,)
+        lbl_rotate = customtkinter.CTkLabel(
+            master=frm_tools,
+            text="Rotation",
+            text_font=(16,),
+            fg_color=("white", "gray38"),
+            corner_radius=5
         )
-        self.lbl_rotate.grid(row=1, column=0, sticky="")
-
-        self.lbl_rotate = customtkinter.CTkLabel(
-            master=self._frm_tools,
-            text="Type the angle in degress.",
-        )
-        self.lbl_rotate.grid(row=2, column=0, sticky="n")
+        lbl_rotate.grid(row=1, column=0)
 
         self.ent_rotate = customtkinter.CTkEntry(
-            master=self._frm_tools,
+            master=frm_tools,
             width=160,
-            placeholder_text="Rotation in degress..."
+            placeholder_text="Type the angle in degrees..."
         )
-        self.ent_rotate.grid(row=3, column=0, sticky="n")
+        self.ent_rotate.grid(row=3, column=0, padx=20, pady=20)
 
-    def _set_default_configs(self) -> None:
-        """"""
-        self.mnu_appearance.set("Dark")
-        self.slider_1.set(0.2)
-        self.slider_2.set(0.7)
+        btn_rotate = customtkinter.CTkButton(
+            master=frm_tools,
+            text="Rotate",
+            text_font=(16,),
+            command=self.rotate_image,
+            corner_radius=5
+        )
+        btn_rotate.grid(row=4, column=0, padx=20, pady=0)
 
     def btn_event(self):
-        print("Button pressed")
+        print("BUTTON PRESSED!")
 
     def load_image(self) -> None:
         """
@@ -207,7 +217,7 @@ class Tkinter(UIInterface, customtkinter.CTk):
 
         image_name = filedialog.askopenfilename(
             title="Open Image",
-            initialdir=CURRENT_PATH,
+            initialdir=self.CURRENT_PATH,
             filetypes=filetypes
         )
 
@@ -223,12 +233,18 @@ class Tkinter(UIInterface, customtkinter.CTk):
         TODO: FIX image_data type!!
         """
         image = image_data.image
-        image.thumbnail((350, 350))
 
-        tk_image = ImageTk.PhotoImage(image)
+        # TODO: UI CALLING THE IMAGE LIB METHOD. MUST CHANGE IT.
+        # RESIZING THE IMAGE
+        canvas_width = self.canvas.winfo_width()
+        canvas_height = self.canvas.winfo_height()
+        resized_image = image.resize((canvas_width, canvas_height)) 
 
-        self._img_canvas.create_image(10, 10, image=tk_image)
-        self._img_canvas.image = tk_image
+        # DISPLAYING THE IMAGE
+        tk_image = ImageTk.PhotoImage(resized_image)
+
+        self.canvas.create_image(0, 0, image=tk_image, anchor="nw")
+        self.canvas.image = tk_image
 
     def save_image(self) -> None:
         """
@@ -236,7 +252,7 @@ class Tkinter(UIInterface, customtkinter.CTk):
         """
         image_name = filedialog.asksaveasfilename(
             title="Save Image",
-            initialdir=CURRENT_PATH
+            initialdir=self.CURRENT_PATH
         )
 
         image_data = {
@@ -257,8 +273,10 @@ class Tkinter(UIInterface, customtkinter.CTk):
         """
         Method to rotate the image.
         """
+        rotation_angle = self.ent_rotate.get()
+
         image_data = {
-            "rotate_angle": 30
+            "rotation_angle": rotation_angle
         }
 
         controller = RotateImageController()
@@ -266,11 +284,6 @@ class Tkinter(UIInterface, customtkinter.CTk):
 
     def change_appearance_mode(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
-
-    # def on_closing(self, event: int=0):
-    #     """"""
-    #     self.destroy()
-    # TODO: CHECK IF CAN BE REMOVABLE
 
     def _on_closing(self):
         """"""
